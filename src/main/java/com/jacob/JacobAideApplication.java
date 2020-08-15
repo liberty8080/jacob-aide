d4d03d1451797dff4247569470cd324d5825d864
liberty8080 <zhaojiawei233@gmail.com>
2020-08-15 21:34:07 +0800

    项目初始化

---
+++ src/main/java/com/jacob/JacobAideApplication.java
@@ -0,0 +1,13 @@
+package com.jacob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacobAideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacobAideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacob/JacobAideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobAideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
