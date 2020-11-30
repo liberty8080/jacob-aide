b638da15d8ca305402b45f1c32791d542af376d6
liberty8080 <zhaojiawei233@gmail.com>
2020-11-30 09:13:47 +0800

    日志处理

--- src/main/java/com/jacob/common/timing/TimingService.java
+++ src/main/java/com/jacob/common/timing/TimingService.java
@@ -1,12 +1,6 @@
 package com.jacob.common.timing;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.jacob.bot.util.Const;
 import com.jacob.bot.util.MessageSendService;
-import com.jacob.common.entity.Configs;
-import com.jacob.common.entity.DdnsLog;
-import com.jacob.common.service.IConfigsService;
-import com.jacob.common.service.IDdnsLogService;
 import com.jacob.common.service.impl.DdnsLogServiceImpl;
 import com.jacob.common.service.impl.V2SubscribeService;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +12,6 @@ import javax.annotation.Resource;
 import java.io.IOException;
 import java.text.MessageFormat;
 
-import static com.jacob.common.util.IPUtil.getPublicIP;
-import static com.jacob.common.util.IPUtil.updateDDNS;
-
 /**
  * @author jacob
  */
@@ -29,17 +20,16 @@ import static com.jacob.common.util.IPUtil.updateDDNS;
 public class TimingService {
 
 
+    @Autowired
+    public MessageSendService messageSendService;
     @Resource
     private DdnsLogServiceImpl ddnsLogService;
-
     @Resource
     private V2SubscribeService v2Service;
 
-    @Autowired
-    public MessageSendService messageSendService;
-
     /**
      * 定时更新ddns信息
+     *
      * @throws IOException
      */
     @Scheduled(cron = "0 0 0 1/1 * ? ")
@@ -48,20 +38,19 @@ public class TimingService {
     }
 
     @Scheduled(cron = "0 0 0 1/1 * ? ")
-    public void remindV2ExpireDays(){
+    public void remindV2ExpireDays() {
         //剩一周提示
         int remindDay = 7;
         try {
-           int expireDays =  v2Service.getExpireDays();
-            if(expireDays<=remindDay&&expireDays!=-1){
+            int expireDays = v2Service.getExpireDays();
+            if (expireDays != -1 && expireDays <= remindDay) {
                 String message = MessageFormat.format("还有{0}天就过期辣,快tm续费啊", expireDays);
                 messageSendService.sendToChannel(message);
-            }else {
-                log.error("v2信息获取失败");
-                messageSendService.sendToChannel("v2信息获取失败");
+            } else {
+                log.info("还有{}天过期",expireDays);
             }
         } catch (IOException e) {
-            log.error("v2信息获取出错",e);
+            log.error("v2信息获取错误", e);
             messageSendService.sendToChannel("v2信息获取错误");
         }
     }
